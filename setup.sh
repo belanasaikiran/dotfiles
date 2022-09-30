213fc9b399e3571a563d95ba04f257a6afbafb6f
Saikiran Belana <88756154+belanasaikiran@users.noreply.github.com>
2022-09-30 07:47:00 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-## Author : Aditya Shakya (adi1090x)
-## Github : @adi1090x
-#
-## Installer Script
-
 ## Colors ----------------------------
 Color_Off='\033[0m'
 BBlack='\033[1;30m' BRed='\033[1;31m'    BGreen='\033[1;32m' BYellow='\033[1;33m'
@@ -26,24 +21,6 @@ install_fonts() {
 	fi
 	echo -e ${BYellow}"[*] Updating font cache...\n" ${Color_Off}
 	fc-cache
-}
-
-# Install Themes
-install_themes() {
-	if [[ -d "$ROFI_DIR" ]]; then
-		echo -e ${BPurple}"[*] Creating a backup of your rofi configs..." ${Color_Off}
-		mv "$ROFI_DIR" "${ROFI_DIR}.${USER}"
-	fi
-	echo -e ${BBlue}"[*] Installing rofi configs..." ${Color_Off}
-	{ mkdir -p "$ROFI_DIR"; cp -rf $DIR/files/* "$ROFI_DIR"; }
-
-	if [[ -f "$ROFI_DIR/config.rasi" ]]; then
-		echo -e ${BGreen}"[*] Successfully Installed.\n" ${Color_Off}
-		exit 0
-	else
-		echo -e ${BRed}"[!] Failed to install.\n" ${Color_Off}
-		exit 1
-	fi
 }
 
 # Main
